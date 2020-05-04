4ef34cbc70cf48e45c01549a0b34b90c6ed3026d
Jeremy Nelson <swif242@hotmail.com>
2020-05-04 07:26:50 -0700

    created initial directories and files, and initial template code

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Ranch Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Big Mac", false);
